99ba9e7ba87f8479f76c89602d2a6cbc6f2d496c
Pavel Asafov <pavelasaf@yandex.ru>
2023-04-11 00:06:34 +0600

    бургер меню рабочий вариант

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import logoMesto from "../images/logo_Mesto.svg";
-import React from "react";
+import React, { useState } from "react";
+import Media from "react-media";
 import { Link, useNavigate } from 'react-router-dom';
+import MenuButton from './MenuButton';
+import "../blocks/menu/__container/menu__container.css"
+import "../blocks/menu/__container_2/header__container_2.css"
+import "../blocks/menu/__container_2/_open/.header__container_2_open.css"
+import "../blocks/header/__email/__open/header__email_open.css"
 
 export default function Header({ loggedIn, email, onLoggin, link }) {
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function handleMenuClick() {
+    setMenuOpen(!menuOpen);
+  };
 
   function signOut() {
     localStorage.removeItem('token');
@@ -12,37 +23,76 @@ export default function Header({ loggedIn, email, onLoggin, link }) {
   }
 
   return (
-    <header className="header">
-      <img
-        className="header__logo"
-        src={logoMesto}
-        alt="Логотип Место"
-      />
-      {
-        loggedIn
-          ? (
-            <div className="header__container">
-              <p className="header__email">
-                {email}
-              </p>
-              <button
-                type="button"
-                className="header__link"
-                onClick={signOut}
-              >
-                Выйти
-              </button>
+    <Media query={{ maxWidth: 500 }}>
+      {matches =>
+        matches ? (
+          <header className="header">
+
+            {
+              menuOpen ?
+                <>
+                  <p className={`header__email ${menuOpen ? 'header__email_open' : ''}`}>
+                    {email}
+                  </p>
+                  <button
+                    type="button"
+                    className="header__link"
+                    onClick={signOut}
+                  >
+                    Выйти
+                  </button>
+                </>
+                : null
+            }
+
+            <div className={`header__container_2 ${menuOpen ? 'header__container_2_open' : ''}`}>
+              <img
+                className="header__logo"
+                src={logoMesto}
+                alt="Логотип Место"
+              />
+              <MenuButton
+                open={menuOpen}
+                onClick={handleMenuClick}
+              />
             </div>
-          )
-          : (
-            <Link
-              to={link === 'Войти' ? "/sign-in" : "/sign-up"}
-              className="header__enter"
-            >
-              {link}
-            </Link>
-          )
+          </header>
+        ) : (
+          <header className="header">
+            <img
+              className="header__logo"
+              src={logoMesto}
+              alt="Логотип Место"
+            />
+            {
+              loggedIn
+                ? (
+                  <div className="header__container">
+                    <p className="header__email">
+                      {email}
+                    </p>
+                    <button
+                      type="button"
+                      className="header__link"
+                      onClick={signOut}
+                    >
+                      Выйти
+                    </button>
+                  </div>
+                )
+                : (
+                  <Link
+                    to={link === 'Войти' ? "/sign-in" : "/sign-up"}
+                    className="header__enter"
+                  >
+                    {link}
+                  </Link>
+                )
+            }
+          </header>
+        )
+
       }
-    </header>
+    </Media>
   );
 }
